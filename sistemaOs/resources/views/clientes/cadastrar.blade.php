8f012e725f66fc5f30fe970b34813c387a1c89d6
johne <johnywalker696@gmail.com>
2023-12-27 15:25:38 -0300

    criando migrate

--- sistemaOs/resources/views/clientes/cadastrar.blade.php
+++ sistemaOs/resources/views/clientes/cadastrar.blade.php
@@ -7,29 +7,33 @@
     <h1>Cadastrar Clientes</h1>
 
     <form class="row g-3" action="/cliente" method="post">
-        <div class="col-md-6">
+        <div class="col-md-4">
             <label for="nome" class="form-label">Nome</label>
             <input type="text" class="form-control" id="nome" name="nome">
         </div>
-        <div class="col-md-6">
-            <label for="endereco" class="form-label">Endereço</label>
-            <input type="text" class="form-control" id="endereco" placeholder=" Qnn 2 conjunto H casa 11" name="nome">
+        <div class="col-md-4">
+            <label for="nome" class="form-label">Cpf</label>
+            <input type="text" class="form-control" id="cpf" name="cpf">
         </div>
-        <div class="col-12">
+        <div class="col-md-4">
+            <label for="endereco" class="form-label">Endereço</label>
+            <input type="text" class="form-control" id="endereco" placeholder=" Qnn 2 conjunto H casa 11" name="endereco">
+        </div>
+        <div class="col-6">
             <label for="email" class="form-label">Email</label>
-            <input type="text" class="form-control" id="email" name="nome">
+            <input type="text" class="form-control" id="email" name="email">
         </div>
         <div class="col-md-6">
             <label for="telefone" class="form-label">Telefone</label>
-            <input type="text" class="form-control" id="telefone" name="nome">
+            <input type="text" class="form-control" id="telefone" name="email">
         </div>
         <div class="col-md-4">
             <label for="datanasc" class="form-label">Data de Nascimento</label>
-            <input type="date" class="form-control" id="datanasc" name="nome">
+            <input type="date" class="form-control" id="datanasc" name="datanasc">
         </div>
         <div class="col-md-2">
             <label for="cep" class="form-label">CEP</label>
-            <input type="text" class="form-control" id="cep" name="nome">
+            <input type="text" class="form-control" id="cep" name="cep">
         </div>
         <div class="col-12">
             <button type="submit" class="btn btn-primary">Cadastrar</button>
